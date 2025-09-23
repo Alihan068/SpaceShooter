20ade7495b6fdda7ac03861e9cc4af6c3629a116
Alihan <alihankomurcuoglu@gmail.com>
2025-09-23 10:33:54 +0300

    Improvements

---
+++ Assets/Scripts/SpriteScroller.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    [SerializeField] Vector2 scrollSpeed;
+
+    Vector2 offset;
+    Material material;
+
+    private void Awake() {
+        material = GetComponent<SpriteRenderer>().material;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        offset = scrollSpeed * Time.deltaTime;
+        material.mainTextureOffset += offset;
+    }
+}
