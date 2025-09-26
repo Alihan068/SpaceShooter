fb24da560a02914580ba57d5396d622783d036ac
Alihan <alihankomurcuoglu@gmail.com>
2025-09-26 10:44:49 +0300

    Improvements
    HomingFinal

--- Assets/Scripts/HomingMissle.cs
+++ Assets/Scripts/HomingMissle.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 public class HomingMissle : MonoBehaviour {
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float homingTimer = 10f;
+    [SerializeField] float turnSpeed = 100f;
 
     bool chase;
 
     Transform target;
+    Rigidbody2D rb2d;
 
     void OnEnable() {
         target = FindFirstObjectByType<PlayerControler>().transform;
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,9 +22,20 @@ public class HomingMissle : MonoBehaviour {
     }
 
     void TargetHoming() {
-        var step = moveSpeed * Time.deltaTime; // calculate distance to move
-        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
-        transform.LookAt(target.position);
+        if (target == null) {
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+        }
+        else {
+            Vector2 direction = (Vector2)target.position - rb2d.position;
+
+            direction.Normalize();
+
+            float rotateAmount = Vector3.Cross(direction, transform.up).z;
+
+            rb2d.angularVelocity = -rotateAmount * turnSpeed;
+
+            rb2d.linearVelocity = transform.up * moveSpeed;
+        }
 
     }
 
